a82911881aa91591f11880576194ad8748cfec40
mhchia <j84255801912@gmail.com>
2017-11-14 17:58:31 +0800

    Refactor

--- sharding/visualization.py
+++ sharding/visualization.py
@@ -30,6 +30,9 @@ def get_shorten_hash(hash_bytes32):
 
 class Record(object):
 
+    TYPE_BLOCK = 0
+    TYPE_COLLATION = 1
+
     def __init__(self):
         self.collations = defaultdict(dict)
         # 'label' -> 'node_name'
@@ -39,6 +42,8 @@ class Record(object):
         # [{'shard_id', 'startgas', 'gasprice', 'to', 'value', 'data'}, ...]
         self.receipts = []
 
+        self.node_type = {}
+        # self.node_events = defaultdict(lambda: dict([('node_type', None), ('label_obj_list', [])]))
         self.node_events = defaultdict(list)
 
         self.blocks = {}
@@ -66,39 +71,58 @@ class Record(object):
         return "{}{}".format(label, number)
 
 
-    def add_event_by_node(self, node_hash, event, number):
+    def add_event_by_node(self, node_hash, event, number, node_type):
         label = self.mk_event_label(event, number)
         self.tx_label_node_map[label] = node_hash
         prev_label = self.get_prev_label(label)
         if prev_label is not None:
             prev_label_node_hash = self.tx_label_node_map[prev_label]
-            index = get_shorten_hash(node_hash) + ':' + label
-            value = get_shorten_hash(prev_label_node_hash) + ':' + prev_label
-            self.node_label_map[index] = value
-        self.node_events[node_hash].append(label)
+        else:
+            prev_label_node_hash = None
+            # index = get_shorten_hash(node_hash) + ':' + label
+            # value = get_shorten_hash(prev_label_node_hash) + ':' + prev_label
+            # self.node_label_map[index] = value
+        self.node_type[node_hash] = node_type
+        obj = {
+            'label': label,
+            'prev_label': prev_label,
+            'prev_label_node_hash': prev_label_node_hash,
+        }
+        self.node_events[node_hash].append(obj)
+
+
+    def add_event_in_block(self, node_hash, event, number):
+        self.add_event_by_node(node_hash, event, number, self.TYPE_BLOCK)
+
+
+    def add_event_in_collation(self, node_hash, event, number):
+        self.add_event_by_node(node_hash, event, number, self.TYPE_COLLATION)
 
 
     def add_add_header_by_node(self, node_hash, number):
-        self.add_event_by_node(node_hash, LABEL_ADD_HEADER, number)
+        # self.add_event_in_block(node_hash, LABEL_ADD_HEADER, number)
+        pass
 
 
     def add_deposit_by_node(self, node_hash, number):
-        self.add_event_by_node(node_hash, LABEL_DEPOSIT, number)
+        self.add_event_in_block(node_hash, LABEL_DEPOSIT, number)
 
 
     def add_withdraw_by_node(self, node_hash, number):
-        self.add_event_by_node(node_hash, LABEL_WITHDRAW, number)
+        self.add_event_in_block(node_hash, LABEL_WITHDRAW, number)
 
 
     def add_receipt_by_node(self, node_hash, number):
-        self.add_event_by_node(node_hash, LABEL_RECEIPT, number)
+        self.add_event_in_block(node_hash, LABEL_RECEIPT, number)
 
 
     def add_receipt_consuming_by_node(self, node_hash, number):
-        self.add_event_by_node(node_hash, LABEL_RECEIPT_CONSUMING, number)
+        self.add_event_in_collation(node_hash, LABEL_RECEIPT_CONSUMING, number)
 
 
     def get_tx_labels_from_node(self, node_hash):
+        if node_hash not in self.node_events.keys():
+            return []
         return self.node_events[node_hash]
 
 
@@ -201,62 +225,91 @@ class ShardingVisualization(object):
         self.g.edge(node, prev_node, style='dashed', constraint='false')
 
 
-    def draw_block(self, prev_hash, current_hash, txs, height):
+    def draw_block(self, current_hash, prev_hash, label_edges, height):
+        prev_node_name = self.get_node_name_from_hash(prev_hash)
+        node_name = self.get_node_name_from_hash(current_hash)
         if self.draw_in_period:
             caption = '{}'.format(height)
         else:
-            caption = 'B{}: {}'.format(height, current_hash)
-        self.draw_struct(prev_hash, current_hash, height, txs, 'record', caption)
+            caption = 'B{}: {}'.format(height, node_name)
+        self.draw_struct(node_name, prev_node_name, height, label_edges, 'record', caption)
 
 
-    def draw_collation(self, prev_hash, current_hash, height, order, txs, is_valid=True):
+    def draw_collation(self, current_hash, prev_hash, label_edges, height, order, is_valid=True):
+        prev_node_name = self.get_node_name_from_hash(prev_hash)
+        node_name = self.get_node_name_from_hash(current_hash)
         caption = 'C' if is_valid else 'IC'
-        caption += '({}, {}): {}'.format(height, order, current_hash)
-        self.draw_struct(prev_hash, current_hash, height, txs, 'Mrecord', caption)
+        caption += '({}, {}): {}'.format(height, order, node_name)
+        self.draw_struct(node_name, prev_node_name, height, label_edges, 'Mrecord', caption)
         if not is_valid:
-            self.g.node(current_hash, color='red', style='filled')
+            self.g.node(node_name, color='red', style='filled')
 
 
-    def draw_struct(self, prev_hash, current_hash, height, txs, shape, caption):
+    def get_node_name_from_hash(self, node_hash):
+        if isinstance(node_hash, bytes):
+            node_hash = get_shorten_hash(node_hash)
+        if self.draw_in_period and \
+                node_hash in self.block_shorten_hash_to_period.keys():
+            node_hash = self.block_shorten_hash_to_period[node_hash]
+        return node_hash
+
+
+    def get_labels_from_node(self, node_hash):
+        '''returns a list of edge pairs from events in the node `node_hash` to other events
+            e.g. [((current_hash, label), (prev_hash, prev_label)), ...]
+        '''
+        label_obj_list = self.record.get_tx_labels_from_node(node_hash)
+        node_name = self.get_node_name_from_hash(node_hash)
+        labels = []
+        for label_obj in label_obj_list:
+            # 'lable': label, 'prev_label': prev_label,
+            # 'prev_label_node_hash': prev_label_node_hash,
+            label = label_obj['label']
+            prev_label = label_obj['prev_label']
+            prev_label_node_name = self.get_node_name_from_hash(label_obj['prev_label_node_hash'])
+            current_label_index = (node_name, label)
+            if prev_label is None:
+                prev_label_index = None
+            else:
+                prev_label_index = (prev_label_node_name, prev_label)
+            edge = (current_label_index, prev_label_index)
+            labels.append(edge)
+        return labels
+
+
+    def draw_struct(self, node_name, prev_node_name, height, label_edges, shape, caption):
         # assert len(txs) <= self.NUM_TX_IN_BLOCK
+
         assert isinstance(height, int)
-        prev_label = '<prev> prev: \n {}'.format(prev_hash)
+        prev_label = '<prev> prev: \n {}'.format(prev_node_name)
+        label_list = [item[0][1] for item in label_edges]
         txs_label = '{'
-        # remove add_header event if it is in `draw_in_period` mode
-        txs = [event_label for event_label in txs if LABEL_ADD_HEADER not in event_label]
         for i in range(self.NUM_TX_IN_BLOCK):
-            if i >= len(txs):
+            if i >= len(label_list):
                 txs_label += '<tx{}> '.format(i)
                 txs_label += self.EMPTY_TX
             else:
-                txs_label += '<{}> '.format(txs[i])
-                txs_label += txs[i]
+                txs_label += '<{}> '.format(label_list[i])
+                txs_label += label_list[i]
             if i != self.NUM_TX_IN_BLOCK - 1:
                 txs_label += ' | '
         txs_label += '}'
         # label = '{ %s | %s | %s }' % (hash_label, txs_label, prev_label)
-        if len(txs) != 0:
-            label = '{ %s | %s }' % (caption, txs_label)
+        if len(label_list) != 0:
+            struct_label = '{ %s | %s }' % (caption, txs_label)
         else:
-            label = '{ %s }' % caption
-        self.g.node(current_hash, label, shape=shape)
-        self.g.edge(current_hash, prev_hash) # , weight=weight)
+            struct_label = '{ %s }' % caption
+        self.g.node(node_name, struct_label, shape=shape)
+        self.g.edge(node_name, prev_node_name) # , weight=weight)
 
         # draw event edges
-        for label in txs:
-            label_index = current_hash + ':' + label
-            if self.draw_in_period and current_hash in self.block_shorten_hash_to_period.keys():
-                label_index = self.block_shorten_hash_to_period[current_hash] + ':' + label
-            try:
-                prev_label_index = self.record.node_label_map[label_index]
-                prev_hash, prev_label = prev_label_index.split(':')
-                if self.draw_in_period and prev_hash in self.block_shorten_hash_to_period.keys():
-                    prev_label_index = self.block_shorten_hash_to_period[prev_hash] + ':' + prev_label
-                if (LABEL_ADD_HEADER in label) or (LABEL_ADD_HEADER in prev_label):
-                    continue
-                self.draw_event_edge(label_index, prev_label_index)
-            except:
-                pass
+        for edge in label_edges:
+            label_index, prev_label_index = edge
+            label_index_str = '{}:{}'.format(label_index[0], label_index[1])
+            if prev_label_index is None:
+                continue
+            prev_label_index_str = '{}:{}'.format(prev_label_index[0], prev_label_index[1])
+            # self.draw_event_edge(label_index_str, prev_label_index_str)
 
 
     def draw_mainchain(self, chain):
@@ -279,26 +332,28 @@ class ShardingVisualization(object):
             if prev_block is None:
                 prev_block_hash = self.mainchain_caption
             else:
-                prev_block_hash = get_shorten_hash(prev_block.header.hash)
-                self.block_shorten_hash_to_period[prev_block_hash] = str(prev_period)
+                prev_block_hash = prev_block.header.hash
+                self.block_shorten_hash_to_period[get_shorten_hash(prev_block_hash)] = \
+                    str(prev_period)
 
-            current_block_hash = get_shorten_hash(current_block.header.hash)
-            self.block_shorten_hash_to_period[current_block_hash] = str(current_period)
-            tx_labels = self.record.get_tx_labels_from_node(current_block.header.hash)
-            tx_labels_in_current_period = tx_labels + tx_labels_in_current_period
+            current_block_hash = current_block.header.hash
+            self.block_shorten_hash_to_period[get_shorten_hash(current_block_hash)] = \
+                str(current_period)
+            label_edges = self.get_labels_from_node(current_block_hash)
+            tx_labels_in_current_period = label_edges + tx_labels_in_current_period
             if not self.draw_in_period:
                 self.draw_block(
-                    prev_block_hash,
                     current_block_hash,
-                    tx_labels,
+                    prev_block_hash,
+                    label_edges,
                     current_block.header.number,
                 )
                 self.layers[current_block_hash] = []
             elif current_block.header.number % chain.env.config['PERIOD_LENGTH'] == 0:
                 self.draw_block(
-                    str(prev_period),
-                    str(current_period),
-                    tx_labels_in_current_period,
+                    current_block_hash,
+                    prev_block_hash,
+                    label_edges,
                     current_period,
                 )
                 tx_labels_in_current_period = []
@@ -317,12 +372,10 @@ class ShardingVisualization(object):
                 if collation_hash == self.GENESIS_HASH:
                     continue
                 height, order = self.record.get_collation_coordinate_by_hash(collation_hash)
-                name = get_shorten_hash(collation_hash)
-                prev_name = collation.header.parent_collation_hash
-                if prev_name == self.GENESIS_HASH:
-                    prev_name = shardchain_caption
-                else:
-                    prev_name = get_shorten_hash(prev_name)
+                name = self.get_node_name_from_hash(collation_hash)
+                prev_hash = collation.header.parent_collation_hash
+                if prev_hash == self.GENESIS_HASH:
+                    prev_hash = shardchain_caption
                 period_start_prevhash = get_shorten_hash(
                     collation.header.period_start_prevhash,
                 )
@@ -330,11 +383,9 @@ class ShardingVisualization(object):
                     self.layers[str(collation.header.expected_period_number)].append(name)
                 else:
                     self.layers[period_start_prevhash].append(name)
-                # g.edge(name, prev_name)
-                # g.node(name, label=label)#, shape='Mrecord')
-                tx_labels = record.get_tx_labels_from_node(collation.header.hash)
+                label_edges = self.get_labels_from_node(collation_hash)
                 is_valid = self.record.is_collation_valid(collation_hash)
-                self.draw_collation(prev_name, name, height, order, tx_labels, is_valid)
+                self.draw_collation(collation_hash, prev_hash, label_edges, height, order, is_valid)
 
 
     def add_rank(self, node_list, rank='same'):
@@ -359,7 +410,6 @@ class ShardingVisualization(object):
     def draw(self):
         self.draw_mainchain(self.mainchain)
         self.draw_shardchains(self.record)
-        print(self.layers)
         self.set_rank(self.layers)
 
         print(self.g.source)
